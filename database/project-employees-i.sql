684cca538179257931a5001b319ed0fda5cd829e
Kaushal Raj <kaushalraj7017@gmail.com>
2026-09-20 15:27:38 +0530

    Synchronize existing submission history (3/3)

---
+++ database/project-employees-i.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select p.project_id, ifnull(round(sum(e.experience_years)/count(p.employee_id),2),0) as average_years from Project p left join
+Employee e on p.employee_id =e.employee_id
+group by project_id
